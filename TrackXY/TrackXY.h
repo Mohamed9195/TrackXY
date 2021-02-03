ab1dcd1bc2204b82cfcfe7773ae37dbdbe1182a6
Mohamed Hashem <mohamedhashem@xtrava.co>
2021-02-03 23:45:27 +0200

    Initial Commit

---
+++ TrackXY/TrackXY.h
@@ -0,0 +1,18 @@
+//
+//  TrackXY.h
+//  TrackXY
+//
+//  Created by mohamed hashem on 03/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrackXY.
+FOUNDATION_EXPORT double TrackXYVersionNumber;
+
+//! Project version string for TrackXY.
+FOUNDATION_EXPORT const unsigned char TrackXYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrackXY/PublicHeader.h>
+
+
